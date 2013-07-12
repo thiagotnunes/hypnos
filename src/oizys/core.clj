1a06c0f06fe4db310ed9f8f09f18931370a9474c
Thiago Nunes <thiagotnunes@gmail.com>
2013-07-12 16:56:59 +0200

    Can resolve one level lets.

--- src/oizys/core.clj
+++ src/oizys/core.clj
@@ -1,28 +1,67 @@
 (ns oizys.core)
 
-(defn- third [coll]
-  (nth coll 2))
+(declare equality-checker)
 
-(defn- split-checks [body]
-  (partition 3 body))
+(def checkers {'=> (var equality-checker)})
 
-(defn- actual [check]
-  (first check))
+(defn- present? [element coll]
+  (when (coll? coll)
+        (some #{element} coll)))
 
-(defn- expected [check]
-  (third check))
-
-(defn- evaluate [expr]
-  (eval expr))
-
-(defn- check [actual expected]
+(defn check [actual expected checker-fn]
   (let [evaluated-actual (evaluate actual)
         evaluated-expected (evaluate expected)]
-    (if (= evaluated-actual evaluated-expected)
+    (if (checker-fn evaluated-actual evaluated-expected)
       true
-      (throw (AssertionError. (str (first actual) " != " (first expected)))))))
+      (throw (AssertionError. (str "Expected " actual " = " expected))))))
+
+(defn equality-checker [actual expected]
+  (= actual expected))
+
+(defn ->symbol [fn-var]
+  (let [fn-meta (meta fn-var)
+        namespace-name (-> fn-meta :ns ns-name)
+        function-name (:name fn-meta)]
+    (symbol (str namespace-name "/" function-name))))
+
+(defn- parse-expression [body]
+  (let [index-of-checker (.indexOf body '=>)
+        check-start-position (- index-of-checker 1)]
+    (split-at check-start-position body)))
+
+(defn- statements [parsed-expression]
+  (->> parsed-expression
+       second
+       (remove #{'=>})
+       (into [])))
+
+(defn- checker [parsed-expression]
+  (->> parsed-expression
+       second
+       second))
+
+(defn- context [parsed-expression]
+  (->> parsed-expression
+       first))
+
+(defn- checker-symbol-for [checker]
+  (-> checker
+      checkers
+      ->symbol))
+
+(defn- resolve-expression [expression]
+  (if (present? '=> expression)
+    (let [parsed-expression (parse-expression expression)
+          context (context parsed-expression)
+          checker (checker parsed-expression)
+          statements (statements parsed-expression)
+          checker-args (conj statements (checker-symbol-for checker))]
+      (concat context [(list 'apply (->symbol (var check)) checker-args)]))
+    expression))
+
+(defn- resolve-fact [body]
+  (let [result (resolve-expression body)]
+    (map resolve-expression result)))
 
 (defmacro fact [description & body]
-  (let [checks# (split-checks body)]
-    (doseq [check# checks#]
-      (check (actual check#) (expected check#)))))
+  (first (resolve-fact body)))
--- test/oizys/core_test.clj
+++ test/oizys/core_test.clj
@@ -2,7 +2,16 @@
   (:require
    [oizys.core :refer :all]))
 
-(fact "these should pass"
-      1 => 1
-      (+ 1 1) => 2
-      2 => (+ 1 1))
+(fact "simple equality test"
+      1 => 1)
+
+(fact "simple expression test"
+      (+ 1 1) => 2)
+
+(fact "nested expressions test"
+      (let [a 1
+            b 2]
+        (+ a a) => b))
+
+; ((let [a 1 b 2] (+ a a) => b))
+; (eval (let [a 1 b 2] (check (+ a a) b))
