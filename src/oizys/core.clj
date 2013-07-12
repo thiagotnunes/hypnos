f00a0a6cfeb36ba5f4924afbf703c92b70de1ef9
Thiago Nunes <thiagotnunes@gmail.com>
2013-07-12 10:57:52 +0200

    Simple checking of two values.

--- src/oizys/core.clj
+++ src/oizys/core.clj
@@ -1,1 +1,25 @@
 (ns oizys.core)
+
+(defn- split-at-checker [body]
+  (split-at 1 body))
+
+(defn- actual [body]
+  (->> body
+       split-at-checker
+       first))
+
+(defn- expected [body]
+  (->> body
+       split-at-checker
+       second
+       (drop 1)))
+
+(defn- check [actual expected]
+  (if (= actual expected)
+    true
+    (throw (AssertionError. (str (first actual) " != " (first expected))))))
+
+(defmacro fact [description & body]
+  (let [actual (actual body)
+        expected (expected body)]
+    (check actual expected)))
--- test/oizys/core_test.clj
+++ test/oizys/core_test.clj
@@ -1,3 +1,9 @@
 (ns oizys.core-test
   (:require
    [oizys.core :refer :all]))
+
+(fact "this should pass"
+      1 => 1)
+
+(fact "this should fail"
+      1 => 2)
