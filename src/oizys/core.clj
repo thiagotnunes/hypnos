c58da902e1fec5ec693d37e288d4f5c7545dfb84
Thiago Nunes <thiagotnunes@gmail.com>
2013-08-01 13:54:45 +0200

    Adding form namespace / refactoring.

--- src/oizys/core.clj
+++ src/oizys/core.clj
@@ -1,75 +1,18 @@
 (ns oizys.core
   (:require
    [oizys.line-position :as position]
-   [oizys.assertion     :as assertion]
+   [oizys.form          :as form]
    [clojure.zip         :as zip]))
-
-(defn- traverse [form pred func]
-  (letfn [(traverse-form [form]
-            (if (zip/end? form)
-              form
-              (if (pred (zip/node form))
-                (recur (func form))
-                (recur (zip/next form)))))]
-    (-> form
-        zip/seq-zip
-        traverse-form
-        zip/root)))
-
-(defn line-for [assertion]
-  (-> assertion
-      meta
-      :line))
-
-(defn- remove-left [form]
-  (-> form zip/left zip/remove))
-
-(defn- remove-right [form]
-  (-> form zip/right zip/remove))
-
-(defn- assertion->function [form]
-  (let [actual (-> form zip/left zip/node)
-        assertion-symbol (zip/node form)
-        expected (-> form zip/right zip/node)]
-    (-> form
-        remove-left
-        zip/next
-        remove-right
-        (zip/insert-left (with-meta `(apply ~#'assertion/confirm ~[actual
-                                                                   expected
-                                                                   (assertion/assertions assertion-symbol)
-                                                                   (line-for assertion-symbol)])
-                           {:oizys-assertion true}))
-        zip/remove)))
-
-(defn- assertions->functions [form]
-  (traverse form
-            assertion/assertions
-            assertion->function))
 
 (defn- remove-description [body]
   (drop 2 body))
-
-(defn- assertion->with-error-handling [form errors]
-  (let [assertion (zip/node form)]
-    (-> form
-        (zip/insert-left (with-meta `(swap! ~errors conj ~assertion)
-                           {:oizys-assertion-error-handling true}))
-        zip/remove)))
-
-(defn- assertions->with-error-handling [form]
-  (let [errors (gensym "errors__")]
-    `(let [~errors (atom [])]
-       ~@(traverse form
-                   #(-> % meta :oizys-assertion)
-                   #(assertion->with-error-handling % errors)))))
 
 (defmacro fact [& _]
   (-> &form
       remove-description
       position/add-line-number-to-assertions
-      assertions->functions
-      assertions->with-error-handling))
+      form/assertions->functions
+      form/assertions->with-error-handling))
 
 (defmacro facts [& body]
   `(do ~@(drop 2 &form)))
---
+++ src/oizys/form.clj
@@ -0,0 +1,61 @@
+(ns oizys.form
+  (:require
+   [oizys.assertion :as assertion]
+   [clojure.zip     :as zip]))
+
+(defn- traverse [form pred func]
+  (letfn [(traverse-form [form]
+            (if (zip/end? form)
+              form
+              (if (pred (zip/node form))
+                (recur (func form))
+                (recur (zip/next form)))))]
+    (-> form
+        zip/seq-zip
+        traverse-form
+        zip/root)))
+
+(defn- line-for [assertion]
+  (-> assertion
+      meta
+      :line))
+
+(defn- remove-left [form]
+  (-> form zip/left zip/remove))
+
+(defn- remove-right [form]
+  (-> form zip/right zip/remove))
+
+(defn- assertion->function [form]
+  (let [actual (-> form zip/left zip/node)
+        assertion-symbol (zip/node form)
+        expected (-> form zip/right zip/node)]
+    (-> form
+        remove-left
+        zip/next
+        remove-right
+        (zip/insert-left (with-meta `(apply ~#'assertion/confirm ~[actual
+                                                                   expected
+                                                                   (assertion/assertions assertion-symbol)
+                                                                   (line-for assertion-symbol)])
+                           {:oizys-assertion true}))
+        zip/remove)))
+
+(defn- assertion->with-error-handling [form errors]
+  (let [assertion (zip/node form)]
+    (-> form
+        (zip/insert-left (with-meta `(swap! ~errors conj ~assertion)
+                           {:oizys-assertion-error-handling true}))
+        zip/remove)))
+
+(defn assertions->with-error-handling [form]
+  (let [errors (gensym "errors__")]
+    `(let [~errors (atom [])]
+       ~@(traverse form
+                   #(-> % meta :oizys-assertion)
+                   #(assertion->with-error-handling % errors)))))
+
+(defn assertions->functions [form]
+  (traverse form
+            assertion/assertions
+            assertion->function))
