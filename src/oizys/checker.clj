3ec69bd2071920f35f83695c45fb7166fb56f1fb
Thiago Nunes <thiagotnunes@gmail.com>
2013-08-16 15:50:14 +0200

    Using macro to define own checkers.

--- src/oizys/checker.clj
+++ src/oizys/checker.clj
@@ -1,18 +1,15 @@
 (ns oizys.checker)
 
-(defn #^{:oizys-checker-fn true} equal [actual expected]
-  (fn []
-    (= actual expected)))
-
-(defn #^{:oizys-checker-fn true} truthy [actual]
-  (fn []
-    actual))
-
-(defn #^{:oizys-checker-fn true} falsey [actual]
-  (fn []
-    (not actual)))
-
 (defmacro defchecker [name params & form]
   `(defn ~(with-meta name (assoc (meta name) :oizys-checker-fn true)) ~params
      (fn []
        ~@form)))
+
+(defchecker equal [actual expected]
+  (= actual expected))
+
+(defchecker truthy [actual]
+  actual)
+
+(defchecker falsey [actual]
+  (not actual))
