162a10840c2075e2470df84211582cd86e1de028
Thiago Nunes <thiagotnunes@gmail.com>
2013-08-27 18:57:08 +0200

    Refactoring the collections matches tests.

--- test/oizys/checkers/collections_test.clj
+++ test/oizys/checkers/collections_test.clj
@@ -3,14 +3,24 @@
    [oizys.core :refer :all]))
 
 (facts "about contains"
-  (fact "for vectors"
-    [1 2 3] => (matches [1 2 3])
-    [1 2 3] => (matches [1 _ 3])
-    [1 2 3] => (matches [1 _ _])
-    [1 2 3] => (matches [_ _ _]))
+  (facts "for vectors"
+    (fact "simple matching"
+      [1 2 3] => (matches [1 2 3]))
 
-  (lie "for vectors"
-    [1 2 3] => (matches [1 2 3 4])
-    [1 2 3] => (matches [1 2 4])
-    [1 2 3] => (matches [1 _ 4])
-    [1 2 3] => (matches [_ _ 4])))
+    (fact "with bindings"
+      (let [a 1
+            b 2
+            c 3]
+        [1 2 3] => (matches [a b c])))
+
+    (fact "with placeholder"
+      [1 2 3] => (matches [1 _ 3])
+      [1 2 3] => (matches [1 _ _])
+      [1 2 3] => (matches [_ _ _]))
+
+    (lie "matches"
+      (let [d 4]
+        [1 2 3] => (matches [1 2 3 d])
+        [1 2 3] => (matches [1 2 4])
+        [1 2 3] => (matches [1 _ 4])
+        [1 2 3] => (matches [_ _ 4])))))
