4b4ead31a59140303697b1e8bd3aba6bff8e6c97
Thiago Nunes <thiagotnunes@gmail.com>
2013-08-27 19:19:36 +0200

    Adding matches test for maps.

--- test/oizys/checkers/collections_test.clj
+++ test/oizys/checkers/collections_test.clj
@@ -33,4 +33,21 @@
         [1 2 3] => (matches [_ _ 4])
         [1 2 3] => (matches [1 2 3 4 & r])
         [1 2 3] => (matches [2 3 & r])
-        [1 2 3] => (matches [1 (:or 3 4 5) 3])))))
+        [1 2 3] => (matches [1 (:or 3 4 5) 3]))))
+
+  (facts "for maps"
+    (fact "simple matching"
+      {:a 1 :b 2} => (matches {:a 1 :b 2})
+      {:d 4 :a 1 :b 2 :c 3} => (matches {:a 1 :b 2}))
+    
+    (fact "with only matching"
+      {:a 1 :b 2} => (matches ({:a 1 :b 2} :only [:a :b])))
+
+    (fact "with placeholder"
+      {:a 1 :b 2} => (matches {:a _})
+      {:nested {:a 1 :b 2}} => (matches {:nested {:a _}}))
+
+    (lie "matches"
+      {:a 1 :b 2} => (matches {:a 1 :b 1})
+      {:a 1 :b 2} => (matches {:c 1 :b 2})
+      {:a 1 :b 2 :c 3} => (matches ({:a 1 :b 2} :only [:a :b])))))
