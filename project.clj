fa0ee034fa172c5454edf1ba263e1755cccc5b82
Thiago Nunes <thiagotnunes@gmail.com>
2013-09-06 13:52:18 +0200

    0.0.1

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject hypnos "0.1.0"
+(defproject hypnos "0.0.1"
   :description "Testing framework for clojure"
   :url "http://www.thiagotnunes.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/algo.monads "0.1.4"]
                  [slingshot "0.10.3"]
