bb50e37e857d3f4553b4eb563d854b06c549175c
Thiago Nunes <thiagotnunes@gmail.com>
2013-09-09 01:20:09 +0200

    Using the new replace-in function for parsing the assertions.

--- src/hypnos/parser/assertion.clj
+++ src/hypnos/parser/assertion.clj
@@ -4,41 +4,20 @@
    [hypnos.assertion      :as assertion]
    [hypnos.result         :as result]
    [hypnos.zip            :as hzip]
+   [hypnos.zip            :refer :all]
    
    [clojure.zip :as zip]
    [potemkin    :as potemkin]))
 
-(defn- remove-actual [form]
-  (-> form
-      hzip/remove-left
-      zip/next))
-
-(defn- remove-expected [form]
-  (-> form
-      hzip/remove-right))
-
-(defn- assertion-function-from [assertion-fn actual expected assertion-symbol]
+(defn- assertion->function [assertion-fn actual expected assertion-symbol]
   (with-meta
     `(apply ~assertion-fn [~(checker/checker->function actual expected)
                            '~assertion-symbol
                            '(~actual ~assertion-symbol ~expected)])
     {:hypnos-assertion true}))
 
-(defn- assertion->function [assertion-fn form]
-  (let [actual (hzip/left-node form)
-        assertion-symbol (zip/node form)
-        expected (hzip/right-node form)]
-    (-> form
-        remove-actual
-        remove-expected
-        (zip/replace (assertion-function-from assertion-fn
-                                              actual
-                                              expected
-                                              assertion-symbol)))))
-
 (def assertion->confirm (partial assertion->function #'assertion/confirm))
 (def assertion->refute (partial assertion->function #'assertion/refute))
-
 
 (defn- with-error-handling [form results]
   (let [assertion (zip/node form)]
@@ -62,11 +41,13 @@
          (result/to-stdout ~description assertion-results##))))))
 
 (defn assertions->confirms [form]
-  (hzip/traverse form
-                 assertion/assertions
-                 assertion->confirm))
+  (replace-in form
+              [left-node right-node current-node]
+              (by assertion->confirm)
+              (where #(assertion/assertions (current-node %)))))
 
 (defn assertions->refutes [form]
-  (hzip/traverse form
-                 assertion/assertions
-                 assertion->refute))
+  (replace-in form
+              [left-node right-node current-node]
+              (by assertion->refute)
+              (where #(assertion/assertions (current-node %)))))
