6b816c5f5eaf96cd2697bdeaa8627fa80191541f
Thiago Nunes <thiagotnunes@gmail.com>
2013-09-09 01:03:05 +0200

    Using the new replace-in function for parsing the descriptions.

--- src/hypnos/parser/description.clj
+++ src/hypnos/parser/description.clj
@@ -1,42 +1,39 @@
 (ns hypnos.parser.description
   (:require
-   [clojure.zip :as zip]
-   [hypnos.zip  :as hzip]))
+   [hypnos.zip :refer :all]))
+
+(def has-description #{'fact 'future-fact 'failing-fact})
 
 (defn description [form]
   (second form))
-
-(defn- should-normalize? [node]
-  (#{'fact 'future-fact 'failing-fact} node))
 
 (defn- normalized? [description]
   (and (map? description)
        (:description description)
        (:nesting description)))
 
-(defn- normalize-description [form]
-  (let [position (zip/right form)
-        description (zip/node position)]
-    (if (normalized? description)
-      form
-      (zip/replace position {:description description :nesting []}))))
+(defn- normalize-description [description]
+  (if (normalized? description)
+    description
+    {:description description :nesting []}))
 
 (defn normalize [form]
-  (hzip/traverse form
-                 should-normalize?
-                 normalize-description))
+  (replace-in form
+              [right-node]
+              (by normalize-description)
+              (where #(has-description (current-node %)))))
 
-(defn- nest-description [description form]
-  (let [position (zip/right form)
-        current-description (zip/node position)]
-    (zip/replace position (update-in current-description
-                                     [:nesting]
-                                     conj
-                                     description))))
+
+(defn- nest-description [description current-description]
+  (update-in current-description
+             [:nesting]
+             conj
+             description))
 
 (defn nest [form]
   (let [description (description form)
         nest-description-fn (partial nest-description description)]
-    (hzip/traverse form
-                   should-normalize?
-                   nest-description-fn)))
+    (replace-in form
+                [right-node]
+                (by nest-description-fn)
+                (where #(has-description (current-node %))))))
