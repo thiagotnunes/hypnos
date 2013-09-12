b82d6d8ffd1d71ae3dcb60f8dbacbe9bf19cfd47
Thiago Nunes <thiagotnunes@gmail.com>
2013-09-12 11:16:12 +0200

    Mocking several functions within a provided block scope.

--- src/hypnos/parser/provided.clj
+++ src/hypnos/parser/provided.clj
@@ -3,31 +3,35 @@
    [velcro.core :refer :all]
    [potemkin :as potemkin]))
 
-(defn revert-to [original-fns]
+(defn revert-to! [original-fns]
   (doseq [[func original-fn] @original-fns]
     (alter-var-root func (fn [_] original-fn))))
 
-(defn mock [func mock-fn original-fns]
-  `(alter-var-root (var ~func)
-                   (fn [original-fn##]
-                     (swap! ~original-fns assoc (var ~func) original-fn##)
-                     (fn [] ~mock-fn))))
+(defn mock! [func return original-fns]
+  (let [func-var (resolve func)]
+    (alter-var-root func-var
+                    (fn [original-fn]
+                      (swap! original-fns assoc func-var original-fn)
+                      (fn [] return)))))
 
-(defn- mocks [form]
+(defn create-mocks! [mocks original-fns]
+  (let [mock-function-pairs (partition 2 mocks)]
+    (doseq [[func return] mock-function-pairs]
+      (mock! (first func) return original-fns))))
+
+(defn mocks! [form]
   (let [mocks (second form)
-        func (-> mocks first first)
-        mock-fn (-> mocks second)
         body (drop 2 form)]
     (potemkin/unify-gensyms
      `(let [original-fns## (atom {})]
-        ~(mock func mock-fn `original-fns##)
+        (create-mocks! '~mocks original-fns##)
         ~@body
-        (revert-to original-fns##)))))
+        (revert-to! original-fns##)))))
 
 (defn provided->mocks [form]
   (replace-in form
               [up-node]
-              (by mocks)
+              (by mocks!)
               (where #(= (current-node %) 'provided))))
 
 
--- test/hypnos/mock_test.clj
+++ test/hypnos/mock_test.clj
@@ -3,9 +3,13 @@
    [hypnos.core :refer :all]))
 
 (defn- one [] 1)
+(defn- two [] 2)
 
 (fact "mocks argumentless function within the scope"
-  (provided [(one) 2]
+  (provided [(one) 2
+             (two) 3]
     (one) => 2
-    (one) => 2)
-  (one) => 1)
+    (one) => 2
+    (two) => 3)
+  (one) => 1
+  (two) => 2)
