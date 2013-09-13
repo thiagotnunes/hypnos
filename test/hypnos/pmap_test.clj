29d2ae8c127aa180938b4073d26936805d12901d
Thiago Nunes <thiagotnunes@gmail.com>
2013-09-13 17:11:40 +0200

    Unpending pmap test

--- test/hypnos/pmap_test.clj
+++ test/hypnos/pmap_test.clj
@@ -11,19 +11,19 @@
 (defn do-something []
   (my-func-caller [1 2 3]))
 
-(future-fact "Test some pcalls"
+(fact "Test some pcalls"
   (provided [(my-func 1) 2
              (my-func 2) 4
              (my-func 3) 6]
     (do-something) => [2 4 6]))
 
-(future-fact "Test some pcalls 2"
+(fact "Test some pcalls 2"
   (provided [(my-func 1) 2
              (my-func 2) 4
              (my-func 3) 6]
     (do-something) => [2 4 6]))
 
-(future-fact "Test some pcalls 3"
+(fact "Test some pcalls 3"
   (provided [(my-func 1) 2
              (my-func 2) 4
              (my-func 3) 6]
