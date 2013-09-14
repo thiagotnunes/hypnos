bd69e88d600047fd7aede8d1fa5250aef2ca6d57
Thiago Nunes <thiagotnunes@gmail.com>
2013-09-15 00:50:26 +0200

    Adding test for mocking third party function.

--- test/hypnos/mock_test.clj
+++ test/hypnos/mock_test.clj
@@ -1,6 +1,7 @@
 (ns hypnos.mock.core-test
   (:require
-   [hypnos.core :refer :all]))
+   [hypnos.core        :refer :all]
+   [support.calculator :as calc]))
 
 (defn- one [] 1)
 (defn- two [] 2)
@@ -29,3 +30,7 @@
     (with-args 1) => [:mocked-with-1]
     (with-args 2) => [:mocked-with-2])
   (with-args 2) => [:with-args 2])
+
+(fact "mock function in another namespace"
+  (provided [(calc/plus 2 2) 5]
+    (calc/plus 2 2) => 5))
