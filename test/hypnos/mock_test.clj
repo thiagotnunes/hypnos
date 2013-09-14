59ff822feded03dbc7269ced2698fb3124ba86b7
Thiago Nunes <thiagotnunes@gmail.com>
2013-09-15 00:46:31 +0200

    fixing original mocked function.

--- test/hypnos/mock_test.clj
+++ test/hypnos/mock_test.clj
@@ -15,10 +15,17 @@
   (two) => 2)
 
 (defn with-args [& args]
-  (concat :with-args args))
+  (concat [:with-args] args))
 
 (fact "mocks function with the given arguments"
   (provided [(with-args 1) [:mocked-with-1]
              (with-args 2) [:mocked-with-2]]
     (with-args 1) => [:mocked-with-1]
     (with-args 2) => [:mocked-with-2]))
+
+(fact "mocks function with the given arguments"
+  (provided [(with-args 1) [:mocked-with-1]
+             (with-args 2) [:mocked-with-2]]
+    (with-args 1) => [:mocked-with-1]
+    (with-args 2) => [:mocked-with-2])
+  (with-args 2) => [:with-args 2])
